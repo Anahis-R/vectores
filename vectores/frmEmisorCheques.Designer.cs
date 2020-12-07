80e5aaac6682f71858b3f21060afa5d1a7909346
ANAHIS MARIN <anahis.marin@pucese.edu.ec>
2020-12-07 17:44:54 -0500

    u-modificacion

--- vectores/frmEmisorCheques.Designer.cs
+++ vectores/frmEmisorCheques.Designer.cs
@@ -33,14 +33,16 @@ namespace vectores
             this.btnCalcular = new System.Windows.Forms.Button();
             this.txtResultado = new System.Windows.Forms.TextBox();
             this.txtpaguesealaordende = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtcantidad
             // 
-            this.txtcantidad.Location = new System.Drawing.Point(98, 133);
+            this.txtcantidad.Location = new System.Drawing.Point(190, 123);
             this.txtcantidad.Margin = new System.Windows.Forms.Padding(2);
             this.txtcantidad.Name = "txtcantidad";
-            this.txtcantidad.Size = new System.Drawing.Size(177, 20);
+            this.txtcantidad.Size = new System.Drawing.Size(76, 20);
             this.txtcantidad.TabIndex = 0;
             // 
             // btnCalcular
@@ -67,18 +69,40 @@ namespace vectores
             // 
             // txtpaguesealaordende
             // 
-            this.txtpaguesealaordende.Location = new System.Drawing.Point(98, 86);
+            this.txtpaguesealaordende.Location = new System.Drawing.Point(190, 86);
             this.txtpaguesealaordende.Margin = new System.Windows.Forms.Padding(2);
             this.txtpaguesealaordende.Name = "txtpaguesealaordende";
             this.txtpaguesealaordende.Size = new System.Drawing.Size(177, 20);
             this.txtpaguesealaordende.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(52, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PAGUE A LA ORDEN DE:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(52, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LA CANTIDAD DE:";
+            // 
             // frmEmisorCheques
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(816, 366);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtpaguesealaordende);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.btnCalcular);
@@ -97,5 +121,7 @@ namespace vectores
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.TextBox txtResultado;
         private System.Windows.Forms.TextBox txtpaguesealaordende;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
